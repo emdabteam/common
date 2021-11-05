7ceb56f03f23ff859ecc554aa30dfab3bfcf0e3a
Marcin D <mdabrow05@gmail.com>
2021-11-05 12:39:08 +0100

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 20,
+  green: 20
+};
+
+console.log(color);
+
+export default color;
